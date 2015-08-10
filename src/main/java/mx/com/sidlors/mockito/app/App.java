f6f8347b4f123743457023ab2c255e18ba20fdd4
jhernandezn <sidlors@github.com>
2015-08-10 12:08:31 -0500

    se agrega log4j para logger

---
+++ src/main/java/mx/com/sidlors/mockito/app/App.java
@@ -0,0 +1,16 @@
+package mx.com.sidlors.mockito.app;
+
+import java.util.logging.Logger;
+
+public class App {
+
+	private static final java.util.logging.Logger logger = Logger
+			.getLogger(App.class.getName());
+
+	public static void main(String[] args) {
+
+		logger.info("mesaje de APP");
+
+	}
+
+}
